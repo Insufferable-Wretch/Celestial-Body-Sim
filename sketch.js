e40b9c250166119832de06bd9f7b958c798d9d74
Insufferable-Wretch <111025520+Insufferable-Wretch@users.noreply.github.com>
2024-02-26 10:46:35 -0700

    Added a class to construct celestial bodies, more functions pertaining to multiplicitious celestial simulation on the way (gravitational attraction, collision, basic movement, etc.), manifesting p5.js issues which temporarily negate program functionality (fixing it soon)

--- sketch.js
+++ sketch.js
@@ -1,43 +1,66 @@
-// (Temporary) Object Setup
-let posX, posY;
-let speedX, speedY;
-let diam, rad;
+// In the future, I'll utilize a list of objects for a couple of functions
+// let objects = [];
+let myCanvas;
+let object1;
 
 function setup() {
   myCanvas = createCanvas(400, 400);
 
-  posX = width / 2;
-  posY = height / 2;
-  speedX = 2;
-  speedY = 3;
-  diam = 25;
-  rad = diam / 2;
+  // (Temporary) Object Placement
+  object1 = new Object(width / 2, height / 2, random(1, 5), random(1, 5), random(50, 200), random(30, 90));
 }
 
 function draw() {
   background(50);
-
-  fill(255);
-  ellipse(posX, posY, diam);
-  updatePos();
+  
+  object1.display();
+  object1.updatePos();
 }
 
-function updatePos() {
-  // Contain Object in a Space (the Canvas)
-  if (posX <= rad || posX >= width - rad) {
-    speedX *= -1;
-  }
-  if (posY <= rad || posY >= height - rad) {
-    speedY *= -1;
-  }
+class Object {
+  constructor(posX, posY, velX, velY, mass, diam, rad = diam / 2) {
+    this.posX = posX;
+    this.posY = posY;
 
-  // Update Position
-  posX += speedX;
-  posY += speedY;
+    this.velX = velX;
+    this.velY = velY;
+
+    // The variable 'mass' is not yet implemented in any significant way for the time being
+    this.mass = mass;
+    this.diam = diam;
+    this.rad = rad;
+  }
+  display() {
+    fill(255, 40, 80);
+    ellipse(this.posX, this.posY, this.diam);
+  }
+  // ALL changes to an object's positional potential --- no matter how miniscule --- must reside here
+  updatePos() {
+    // Contain Object in a Space (the Canvas)
+    if (this.posX <= this.rad || this.posX >= 400 - this.rad) {
+      velX *= -1;
+    }
+    if (this.posY <= this.rad || this.posY >= 400 - this.rad) {
+      this.velY *= -1;
+    }
+
+    // Calculate Gravitational Force
+
+    // Update Position
+    this.posX += this.velX;
+    this.posY += this.velY;
+  }
 }
 
-// Space Stuff
+// In the future, I'll allow the user to click upon the canvas and summon a new object
+// function mouseClicked() {
+//   new Object(mouseX, mouseY, random(1, 4), random(1, 4), random(50, 200), random(30, 90));
+// }
+
+// Space Stuff (Mathematics)
 
 // -Equations
 // --Gravitational Attraction Between Two Bodies
-// F = G((m1m2) / d^2)
+// ---F = G((m1m2) / r^2)
+// --Velocity Vector
+// ---V = sqrt(v.x^2 * v.y^2)
